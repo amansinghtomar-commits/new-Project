f585d53c6b40195388a16144bc1715eba403f7e7
Aman Singh <amansingh34906@gmail.com>
2025-12-05 21:05:23 +0530

    Desktop Done

---
+++ script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+    $('.eg-testimonial-slider').slick({
+        slidesToShow: 2,    
+        slidesToScroll: 1,
+        loop: true,
+        arrows: true,
+        dots: true,
+        infinite: true,
+        centerMode: false,
+    });
+});
+

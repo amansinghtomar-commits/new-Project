1bed1b33ee61bba68a8f8fdc9fb6c49aa301cb7b
Aman Singh <amansingh34906@gmail.com>
2025-12-08 01:19:07 +0530

    Changes

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.eg-testimonial-slider').slick({
-        slidesToShow: 2,    
+        slidesToShow: 2,
         slidesToScroll: 1,
         loop: true,
         arrows: true,
         dots: true,
         infinite: true,
         centerMode: false,
+
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
 });
 
